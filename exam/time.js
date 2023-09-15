995cae9740ffa3e6bed660dc61aabd1fc9f4e98e
drlvk <drlvk@users.noreply.github.com>
2023-09-14 22:19:02 -0400

    fix the display

--- exam/time.js
+++ exam/time.js
@@ -1,4 +1,5 @@
-window.onload = displayClock();
+document.addEventListener("DOMContentLoaded", displayClock);
+
 function displayClock(){
   document.getElementById("time").innerText = new Date().toLocaleTimeString("en-US", {hour: "numeric", minute: "2-digit"});
   setTimeout(displayClock, 1000); 
