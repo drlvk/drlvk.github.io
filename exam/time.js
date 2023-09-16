69bfaf538b25e6fe5c7aee4d80432ae8467b5ae6
drlvk <drlvk@users.noreply.github.com>
2023-09-15 22:15:42 -0400

    inline js

--- exam/time.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", displayClock);
-
-function displayClock(){
-  document.getElementById("time").innerText = new Date().toLocaleTimeString("en-US", {hour: "numeric", minute: "2-digit"});
-  setTimeout(displayClock, 1000); 
-}
